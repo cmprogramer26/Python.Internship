453120dee20ea92335f31aa8dffc3a2f731b6a2f
cmprogramer26 <107471792+cmprogramer26@users.noreply.github.com>
2022-07-28 15:23:57 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,17 @@
+#concatenating  two string
+
+var_1="Good Morning "
+var_2="Parag"
+var_3=var_1+var_2
+print(var_3)
+
+
+#string indexing
+
+a="Mahadev"
+print(a[3])
+
+#string slicing
+
+a="Talk Master"
+print(a[:-1:])
